caa2b0618c95dad1384ad7e8adfd802491b4bab0
xcd0 <xcd0@xcd0.com>
2024-02-26 13:53:02 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -x
 cd "$(dirname "$0")"
 rm -rf build*
 mkdir -p build_dynamic_mt build_static_mt build_dynamic_md build_static_md
+
 cd src
 call msvcbuild.bat
 cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_dynamic_mt
@@ -15,3 +15,8 @@ cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_dynami
 call msvcbuild.bat static
 cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_static_md
 cd ..
+
+7z a build_dynamic_mt.7z build_dynamic_mt/*
+7z a build_static_mt.7z  build_static_mt/*
+7z a build_dynamic_md.7z build_dynamic_md/*
+7z a build_static_md.7z  build_static_md/*
