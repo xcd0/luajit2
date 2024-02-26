1dac19cbc7134c2d77f3956df74d5a9a480ad6a5
xcd0 <xcd0@xcd0.com>
2024-02-26 14:38:49 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
 link_opt=$1 # dynamic or static
-mt_opt=$2 #  md or mt
+mt_opt=$2 #	md or mt
 
 opt=""
 
 if [ "$link_opt" == "static" ]; then
-  opt="static"
-  link_opt="static"
+	opt="static"
+	link_opt="static"
 else
-  link_opt="dynamic"
+	opt=""
+	link_opt="dynamic"
 fi
 if [ "$mt_opt" == "md" ]; then
-  sed -i "s;/MT;/MD;g" src/msvcbuild.bat
+	sed -i "s;/MT;/MD;g" src/msvcbuild.bat
+	mt_opt="md"
 else
-  mt_opt="mt"
+	mt_opt="mt"
 fi
+
+out=build_${link_opt}_${mt_opt}
+echo '$opt : '${opt}
+echo '$out : '${out}
 
 cd "$(dirname "$0")"
 pwd
 rm -rf build_*
-mkdir -p build_${link_opt}_${mt_opt}
-
+mkdir -p ${out}
 cd src
-
-cmd.exe /c msvcbuild.bat $opt
-cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_${link_opt}_${mt_opt}
+cmd.exe /c msvcbuild.bat ${opt}
+cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../${out}
 cd ..
-z a build_${link_opt}_${mt_opt}.7z build_${link_opt}_${mt_opt}/*
+z a ${out}.7z ${out}/*
