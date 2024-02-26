a759a9ae320df26aa7ac8a2ca8a623dff963ca72
xcd0 <xcd0@xcd0.com>
2024-02-26 13:46:34 +0900

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+cd "$(dirname "$0")"
+rm -rf build*
+mkdir -p build_dynamic_mt build_static_mt build_dynamic_md build_static_md
+cd src
+call msvcbuild.bat
+cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_dynamic_mt
+call msvcbuild.bat static
+cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_static_mt
+sed -i "s;/MT;/MD;g" msvcbuild.bat
+call msvcbuild.bat
+cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_dynamic_md
+call msvcbuild.bat static
+cp -rf lua51.lib luajit.lib luajit.exe lua51.dll include include ../build_static_md
+cd ..
